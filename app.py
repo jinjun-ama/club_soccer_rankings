cb0463604df995ecde42a673819d0decac47be93
Jin Jun <61722758+jinjun-ama@users.noreply.github.com>
2022-04-12 14:12:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import streamlit as st
+import plotly_express as px
+
+df = pd.read_csv("club_soccer_rankings.csv")
 
 st.title("Global Soccer Club Rankings Overtime")
+
+fig = px.line(
+  df, x="data", y="rating", color="team", render_mode="svg", line_shape="spline"
+)
+
+st.plotly_chart(fig, use_container_width=True)
+
 st.markdown('[Data Credit: FiveThirtyEight Global Soccer Club Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
