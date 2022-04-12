92e547f68ea755ae2dae0b73c5fd308651de3916
Jin Jun <61722758+jinjun-ama@users.noreply.github.com>
2022-04-12 14:03:46 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Global Soccer Club Rankings Overtime")
+st.markdown('[Data Credit: FiveThirtyEight Global Soccer Club Rankings](https://projects.fivethirtyeight.com/soccer-predictions/global-club-rankings/)')
